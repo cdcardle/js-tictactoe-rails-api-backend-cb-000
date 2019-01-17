340009341791f2e181953c92df4bae58fe34458a
Chris Cardle <cdcardle@gmail.com>
2019-01-17 19:40:24 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :edit, :update]
+  before_action :set_game, only: [:show, :update]
 
   def index
     @games = Game.all
